d9acfd6bee06f8682fee944683f8662b45f2e685
Tommy <zhan.tommy@gmail.com>
2026-03-08 21:39:23 +0100

    fail test

--- tests/test_google.py
+++ tests/test_google.py
@@ -4,4 +4,4 @@ from playwright.sync_api import Page, expect
 
 def test_open_chrome(page: Page):
     page.goto("https://www.google.com")
-    expect(page).to_have_title("Google")
+    expect(page).to_have_title("Googlee")
